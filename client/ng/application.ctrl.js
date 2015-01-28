b1d4ba7eb54f51063315e446ded8fb2b851666cf
bailejl <joe.bailey@osgcorp.com>
2015-01-28 14:14:14 +0000

    Updated the code to change the page on logon.

--- client/ng/application.ctrl.js
+++ client/ng/application.ctrl.js
@@ -1,10 +1,11 @@
-angular.module('app').controller('ApplicationCtrl', function ($scope) {
+angular.module('app').controller('ApplicationCtrl', function ($scope, $location) {
     $scope.$on('login', function (_, user) {
-        $scope.currentUser = user
-    })
+        $scope.currentUser = user;
+        $location.path('/');
+    });
     $scope.$on('logout', function (event, msg) {
         $scope.currentUser = null;
 
     })
-})
+});
 
--- client/ng/routes.js
+++ client/ng/routes.js
@@ -1,12 +1,15 @@
 angular.module('app').config(function ($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode( true);
+        $locationProvider.html5Mode(true);
         $routeProvider.when('/', {
             controller: 'PostsCtrl',
             templateUrl: 'posts.html'
         }).when('/register', {
             controller: 'RegisterCtrl',
             templateUrl: 'register.html'
-        }).when('/login', {controller: 'LoginCtrl', templateUrl: 'login.html'})
+        }).when('/login', {
+            controller: 'LoginCtrl',
+            templateUrl: 'login.html'
+        })
     }
 )
 
