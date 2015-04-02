af24c9c163efe2077054cf7d185925ca721b4d10
bailejl <joe.bailey@osgcorp.com>
2015-04-02 09:08:19 +0100

    Last ACL work

--- server/db.js
+++ server/db.js
@@ -1,21 +1,15 @@
 var mongoose = require('mongoose');
-var acl = require('acl');
+var Q = require('q');
+var acl =  require('./security/acl');
 
 var dbVersion = '0.0.1';
 
 var dbUrl = 'mongodb://localhost/asyncmeeting';
-var self = this;
-var db = null;
 
 mongoose.connect(dbUrl, function (err, db) {
     console.log('mongodb connected to ' + dbUrl);
-    self.backend = new acl.mongodbBackend(db, "acl");
-    require('./security/acl');
+    acl.init(db);
 });
-
-exports.getDb = function() {
-
-}
 
 module.exports = mongoose;
 
--- server/security/acl.js
+++ server/security/acl.js
@@ -1,8 +1,17 @@
-var acl = require('acl');
-var db = require('../db');
+var Acl = require('acl');
+//var db = require('../db');
 // TODO finish getting ACL integrated
-/*
-acl = new acl(db.backend);
-acl.allow('GeneralUser', 'MeetingArea', 'CanViewMeetingAreas');
-acl.allow('GeneralUser', 'MeetingArea', 'CanViewMeetings');
- */
+var aclSetup;
+aclSetup = (function () {
+    var init = function (db) {
+        var backend = new Acl.mongodbBackend(db, "acl");
+        var acl = new Acl(backend);
+        acl.allow('GeneralUser', 'MeetingArea', 'CanViewMeetingAreas');
+        acl.allow('GeneralUser', 'MeetingArea', 'CanViewMeetings');
+    };
+    return {
+        init: init
+    }
+})();
+
+module.exports = aclSetup;
