8b02ca8d7e8589bb41dc8a1c0920ccea42b01abb
bailejl <joe.bailey@osgcorp.com>
2015-04-05 08:15:05 -0700

    Added postinstall actions to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var fs = require('fs');
 
-fs.readdirSync(__dirname + '/gulp').forEach(function (task) {
-    require('./gulp/' + task)
+fs.readdirSync(__dirname + '/support/gulp').forEach(function (task) {
+    require('./support/gulp/' + task)
 });
 
 gulp.task('dev', ['css', 'images', 'js', 'html', 'watch:css', 'watch:images', 'watch:js', 'watch:html', 'watch:config', 'dev:server']);
