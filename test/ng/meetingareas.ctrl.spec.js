b532e6a7b25778a77e89b8eb63736672633d3dc7
bailejl <joe.bailey@osgcorp.com>
2015-04-12 01:00:18 -0700

    Fixed  in the Karma tests by loading the module in place of injecting it.

--- test/ng/meetingareas.ctrl.spec.js
+++ test/ng/meetingareas.ctrl.spec.js
@@ -3,23 +3,24 @@ describe('viewing meeting areas', function() {
     var $log;
     var $routeParams;
     var $controller;
-    var $cookies;
     var meetingAreaCtrl;
 
     //beforeEach(module('rcWizard'));
+    beforeEach(function() {
+        module('ngCookies')
+    })
     beforeEach(module(asm.modules.meetingareas.name));
 
-    beforeEach(inject(function(_$rootScope_, _$controller_, _$routeParams_, _$cookies_) {
+    beforeEach(inject(function(_$rootScope_, _$controller_, _$routeParams_) {
         $scope = _$rootScope_;
         //$log = _$log_;
         //$scope = {};
         $controller = _$controller_;
         $routeParams = _$routeParams_;
-        $cookies = _$cookies_;
         meetingAreaCtrl = $controller(asm.modules.meetingareas.controllers.meetingAreasCtrl, {$scope: $scope, $routeParams: {meetingAreaId: '123'}});
     }));
 
     it('should read the current meeting area id from the route parameters', function() {
-       expect($scope.currentMeetingAreaId).to.equal(123);
+       expect($scope.currentMeetingAreaId).to.equal('123');
     });
 });
