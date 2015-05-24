780c7e6a4c0ee0ef46f53baab4009a092aae0ab2
bailejl <joe.bailey@osgcorp.com>
2015-05-24 11:24:25 -0700

    turned off keep alive on protractor, so Jenkins will report properly

--- support/grunt/options/protractor.js
+++ support/grunt/options/protractor.js
@@ -4,7 +4,7 @@
 module.exports = {
     options: {
         configFile: __dirname + "../../../../protractor.conf.js", // Default config file
-        keepAlive: true, // If false, the grunt process stops when the test fails.
+        keepAlive: false, // If false, the grunt process stops when the test fails.
         noColor: false, // If true, protractor will not use colors in its output.
         args: {
             baseUrl: 'localhost:3001'
