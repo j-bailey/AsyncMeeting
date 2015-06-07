b8432a45e394211669d95c4200a6b31e88850d62
Joe Bailey <bailejl@yahoo.com>
2015-06-07 01:21:02 -0700

    Refactored passport init to move serialization function to another file and created tests for the moved functions

---
+++ server/passport/serialization.js
@@ -0,0 +1,18 @@
+/**
+ * Created by jlb on 6/7/15.
+ */
+
+module.exports = function(passport) {
+
+    passport.serializeUser(function (user, done) {
+        var sessionUser = {_id: user._id, name: user.name, email: user.email, roles: user.roles};
+        done(null, sessionUser);
+    });
+
+    passport.deserializeUser(function (sessionUser, done) {
+        // The sessionUser object is different from the user mongoose collection
+        // it's actually req.session.passport.user and comes from the session collection
+        done(null, sessionUser);
+    });
+
+};
---
+++ test/unit/server/passport/serialization.spec.js
@@ -0,0 +1,76 @@
+var User = require('../../../../server/models/user');
+
+describe('serialization', function() {
+    var sandbox;
+
+    beforeEach(function() {
+        sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function() {
+        sandbox.restore();
+    });
+
+    describe('serializeUser', function() {
+        it('should create a session user', function(done) {
+            var email = 'myemail@email.com';
+
+            var userElements = {
+                _id: 1,
+                name: 'Jim Bob',
+                email: email,
+                roles: ['GodMode']
+            };
+            var user = new User(userElements);
+
+            var doneFunc = function(one, two, three) {
+                expect(one).to.equal(null);
+                expect(two).to.deep.equal(userElements);
+                expect(three).to.equal(undefined);
+                done();
+            };
+
+            var useSpy = sandbox.spy(),
+                passport = {
+                serializeUser: function(func) {
+                    func(userElements, doneFunc);
+                },
+                deserializeUser: sandbox.spy(),
+                use: useSpy
+            };
+
+            require('../../../../server/passport/serialization')(passport);
+        });
+        it('should create a deserialize a session user', function(done) {
+            var email = 'myemail@email.com';
+
+            var userElements = {
+                _id: 1,
+                name: 'Jim Bob',
+                email: email,
+                roles: ['GodMode']
+            };
+            var user = new User(userElements);
+
+            var doneFunc = function(one, two, three) {
+                expect(one).to.equal(null);
+                expect(two).to.deep.equal(userElements);
+                expect(three).to.equal(undefined);
+                done();
+            };
+
+            var useSpy = sandbox.spy(),
+                passport = {
+                    serializeUser: sandbox.spy(),
+                    deserializeUser: function(func){
+                        func(userElements, doneFunc)
+                    },
+                    use: useSpy
+                };
+
+            require('../../../../server/passport/serialization')(passport);
+        });
+    });
+});
+
+
