2a4efdab75ff35adbeed2efd66aea8dced88df65
Joe Bailey <bailejl@yahoo.com>
2015-06-13 16:47:59 +0100

    Finished all paths in server ACL file

--- test/unit/server/security/acl.spec.js
+++ test/unit/server/security/acl.spec.js
@@ -3,35 +3,30 @@ describe('security/acl', function () {
     var acl;
 
     var AclAllowStub,
-        AclStub,
-        AclMongoStub,
         fsStub,
         cfgStub,
         mongoDbUrl,
         mongoStub;
 
-    before(function () {
+    beforeEach(function () {
         sandbox = sinon.sandbox.create();
 
         AclAllowStub = sandbox.stub(require('acl').prototype, 'allow');
-        //AclStub = sandbox.stub(require('acl').__proto__, 'constructor');
-        //AclMongoStub = sandbox.stub(require('acl'), 'mongodbBackend');
-        //AclMongoStub.returns(function(){});
         // TODO fix winston stub
         //loggerStub = sandbox.stub(require('winston'), 'info');
         //loggerErrorStub = sandbox.stub(require('winston'), 'error');
         fsStub = sandbox.stub(require('fs'), 'readdirSync');
-        var fileList = ['free-tier-role.js', 'mid-tier-role.js', 'NotARole.js']
+        var fileList = ['free-tier-role.js', 'mid-tier-role.js', 'NotARole.js'];
         fsStub.returns(fileList);
         cfgStub = sandbox.stub(require('config'), 'get');
         mongoDbUrl = 'mongodb://test/test';
         cfgStub.returns(mongoDbUrl);
         mongoStub = sandbox.stub(require('mongodb'), 'connect');
-
+        delete require.cache[require.resolve('../../../../server/security/acl')];
         acl = require('../../../../server/security/acl');
     });
 
-    after(function () {
+    afterEach(function () {
         sandbox.restore();
     });
 
@@ -55,13 +50,31 @@ describe('security/acl', function () {
         });
         it('should throw and error due to db issue', function (done) {
             var dbObj = {bdid:'test'};
-            acl.init();
-
-            mongoStub.args[0][0].should.equal(mongoDbUrl);
             var dbErr = 'cannot connect to DB';
-            expect(mongoStub.args[1][1].bind(null, dbErr, dbObj)).to.throw(dbErr);
+            mongoStub.yields(dbErr, dbObj);
+
+            expect(acl.init.bind()).to.throw(dbErr);
+
             // TODO fix winston issue
             //loggerError.args[0][0].should.equal('Error connecting to database for ACL set up.  ' + dbErr);
+            done();
+        });
+    });
+    describe('Get current instance of Acl', function() {
+        it('should get current instance', function(done){
+            var dbObj = {bdid:'test'};
+            mongoStub.yields(undefined, dbObj);
+            acl.init();
+            //noinspection BadExpressionStatementJS
+            expect(acl.getAcl()).not.to.be.null;
+
+            done();
+        });
+        it('should get current instance when not initialized', function(done){
+            var dbObj = {bdid:'test'};
+            mongoStub.yields(undefined, dbObj);
+            //noinspection BadExpressionStatementJS
+            expect(acl.getAcl()).not.to.be.null;
             done();
         });
     });
