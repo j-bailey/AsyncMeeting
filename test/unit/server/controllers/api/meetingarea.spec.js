e57d338b2964502776a51415eff7675c3b759bf9
Kevin Lee <kglee79@gmail.com>
2015-06-14 00:08:46 -0700

    Finish happy path unit tests for meeting area handler

--- test/unit/server/controllers/api/meetingarea.spec.js
+++ test/unit/server/controllers/api/meetingarea.spec.js
@@ -1,5 +1,6 @@
 var MeetingArea = require('../../../../../server/models/meetingArea');
 var meetingAreaHandler = require('../../../../../server/controllers/api/handlers/meetingAreasHandler');
+var ObjectId = require('mongoose').Types.ObjectId;
 
 describe('meeting area route', function() {
     var sandbox;
@@ -10,6 +11,45 @@ describe('meeting area route', function() {
 
     afterEach(function() {
         sandbox.restore();
+    });
+
+    describe('getMeetingAreasWithParentId', function() {
+        it('should return all meeting areas with the given parent id', function(done) {
+            var id = ObjectId();
+
+            var meetingArea1 = new MeetingArea({
+                title: "Meeting Area Title 1",
+                description: "Meeting Area Description 1",
+                parentMeetingArea: id
+            });
+
+            var meetingArea2 = new MeetingArea({
+                title: "Meeting Area Title 2",
+                description: "Meeting Area Description 2",
+                parentMeetingArea: id
+            });
+
+            var meetingAreas = [meetingArea1, meetingArea2];
+
+            sandbox.stub(MeetingArea.base.Model, 'find').yields(null, meetingAreas);
+
+            var req = { query: { parentId: id } };
+            var res = {
+                status: function(status) {
+                    expect(statusSpy.calledWith(200)).to.equal(true);
+                    return this
+                },
+                json: function(json) {
+                    expect(jsonSpy.calledWith(meetingAreas)).to.equal(true);
+                    done();
+                }
+            };
+
+            var statusSpy = sandbox.spy(res, 'status');
+            var jsonSpy = sandbox.spy(res, 'json');
+
+            meetingAreaHandler.getMeetingAreasWithParentId(req, res);
+        });
     });
 
     describe('getMeetingArea', function() {
@@ -82,40 +122,61 @@ describe('meeting area route', function() {
         });
     });
 
-    //describe('POST \'/\'', function() {
-    //    it('should create a meeting area', function(done) {
-    //        request
-    //            .post('/api/meetingarea')
-    //            .send({
-    //                parentMeetingAreaId: meetingAreaId,
-    //                title: "New Meeting Area",
-    //                description: "New Meeting Area Description"
-    //            })
-    //            .set('Accept', 'application/json')
-    //            .expect('Content-Type', /json/)
-    //            .expect(201)
-    //            .end(function(err, res) {
-    //                var result = JSON.parse(res.text);
-    //                expect(result._id).to.not.be.null;
-    //                MeetingArea.find({}, function(err, meetingAreas) {
-    //                    expect(meetingAreas).to.have.length(2);
-    //                    done();
-    //                });
-    //            });
-    //    });
-    //});
-    //
-    //describe('DELETE \'/\'', function() {
-    //    it('should remove a meeting area', function(done) {
-    //        request
-    //            .delete('/api/meetingarea/' + meetingAreaId)
-    //            .expect(200)
-    //            .end(function(err, res) {
-    //                MeetingArea.find({}, function (err, meetingAreas) {
-    //                    expect(meetingAreas).to.be.empty;
-    //                    done();
-    //                });
-    //            });
-    //    });
-    //});
+    describe('deleteMeetingArea', function() {
+        it('should delete a meeting area', function(done) {
+            var id = ObjectId();
+
+            sandbox.stub(MeetingArea.base.Model, 'findOneAndRemove').yields(null);
+
+            var req = { params: { meetingAreaId: id } };
+            var res = {
+                sendStatus: function(status) {
+                    expect(sendStatusSpy.calledWith(200)).to.equal(true);
+                    done();
+                }
+            };
+
+            var sendStatusSpy = sandbox.spy(res, 'sendStatus');
+
+            meetingAreaHandler.deleteMeetingArea(req, res);
+        });
+    });
+
+    describe('updateMeetingArea', function() {
+        it('should update a meeting area', function(done) {
+            var id = ObjectId();
+
+            var meetingArea = new MeetingArea({
+                title: "My Meeting Area",
+                description: ""
+            });
+
+            sandbox.stub(MeetingArea.base.Model, 'findOne').yields(null, meetingArea);
+            sandbox.stub(meetingArea, 'save').yields(null, meetingArea);
+
+            var req = {
+                params: {
+                    meetingAreaId: id,
+                    title: "My New Meeting Area Title",
+                    description: ""
+                }
+            };
+
+            var res = {
+                status: function(status) {
+                    expect(statusSpy.calledWith(200)).to.equal(true);
+                    return this
+                },
+                json: function(json) {
+                    expect(jsonSpy.calledWith(meetingArea)).to.equal(true);
+                    done();
+                }
+            };
+
+            var statusSpy = sandbox.spy(res, 'status');
+            var jsonSpy = sandbox.spy(res, "json");
+
+            meetingAreaHandler.updateMeetingArea(req, res);
+        });
+    });
 });
