9bf92e9491af4c51bc8e044eade2e13b9bb6f916
Joe Bailey <bailejl@yahoo.com>
2015-06-14 14:48:57 +0100

    Finished unit tests for server user object

--- test/unit/server/models/user.spec.js
+++ test/unit/server/models/user.spec.js
@@ -4,6 +4,7 @@ describe('model/user', function () {
 
     beforeEach(function () {
         sandbox = sinon.sandbox.create();
+        sandbox.stub(process, 'nextTick').yields();
 
         user = require('../../../../server/models/user');
     });
@@ -34,40 +35,47 @@ describe('model/user', function () {
     });
     describe('findPublicUserByUserName', function () {
         it('should return public user information for a given ID', function (done) {
-            var populateStub1 = sandbox.stub(),
+            var userObj = {_id:1, username: "myName"},
+                populateStub1 = sandbox.stub(),
                 populateStub2 = sandbox.stub(),
-                execStub = sandbox.stub(),
                 findByIdStub  = sandbox.stub(user.base.Model, 'findOne');
-            var execStub = sandbox.stub(require('mongoose').__proto__.Query.prototype, 'exec')
+            var execStub = sandbox.stub(require('mongoose').__proto__.Query.prototype, 'exec');
             findByIdStub.returns({populate:populateStub1});
             populateStub1.returns({populate:populateStub2});
             populateStub2.returns({exec:execStub});
-            user.findPublicUserByUserName('UserNameIs');
+
+            execStub.yields(undefined, userObj);
+
+            var findPromise = user.findPublicUserByUserName('UserNameIs');
 
             findByIdStub.args[0][0].should.deep.equal({username:'UserNameIs'});
             findByIdStub.args[0][1].should.equal('username email permissions roles createdOn modifiedOn firstName lastName');
             populateStub1.args[0][0].should.equal('permissions');
             populateStub2.args[0][0].should.equal('roles');
-
+            expect(findPromise).to.eventually.deep.equal(userObj);
             done();
         });
     });
     describe('findPublicUserByEmail', function () {
         it('should return public user information for a given ID', function (done) {
-            var populateStub1 = sandbox.stub(),
+            var userObj = {_id:1, username: "myName"},
+                populateStub1 = sandbox.stub(),
                 populateStub2 = sandbox.stub(),
-                execStub = sandbox.stub(),
                 findByIdStub  = sandbox.stub(user.base.Model, 'findOne');
-            var execStub = sandbox.stub(require('mongoose').__proto__.Query.prototype, 'exec')
+            var execStub = sandbox.stub(require('mongoose').__proto__.Query.prototype, 'exec');
             findByIdStub.returns({populate:populateStub1});
             populateStub1.returns({populate:populateStub2});
             populateStub2.returns({exec:execStub});
-            user.findPublicUserByEmail('MyEmail@email.com');
+
+            execStub.yields(undefined, userObj);
+
+            var findPromise = user.findPublicUserByEmail('MyEmail@email.com');
 
             findByIdStub.args[0][0].should.deep.equal({email:'MyEmail@email.com'});
             findByIdStub.args[0][1].should.equal('username email permissions roles createdOn modifiedOn firstName lastName');
             populateStub1.args[0][0].should.equal('permissions');
             populateStub2.args[0][0].should.equal('roles');
+            expect(findPromise).to.eventually.deep.equal(userObj);
 
             done();
         });
