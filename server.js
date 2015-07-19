150148ebff7db673aa100d2748ecad5eb6f0d5f7
Joe Bailey <bailejl@yahoo.com>
2015-07-19 09:57:34 -0700

    Added clustering with process forking

--- server.js
+++ server.js
@@ -14,92 +14,34 @@ process.on('unacaughtException', function(err){
 require('./config/configSetup');
 var nconf = require('nconf');
 
+var cluster = require('cluster');
+// Ask the number of CPU-s for optimal forking (one fork per CPU)
+var numCPUs = require('os').cpus().length,
+    reservedCPUs = nconf.get('server:reservedCPUs'),
+    singleProcess = nconf.get('server:singleProcess');
 
-var acl = require('./server/security/acl');
-acl.init().then(function() {
+if (singleProcess) {
+    numCPUs = 1;
+} else if (numCPUs - reservedCPUs < 1) {
+    throw new Error('resveredCPUs is too high and does not allow enough CPUs for the application.  ' +
+        'Number of available CPUs is at: ' + numCPUs - reservedCPUs);
+} else {
+    numCPUs = numCPUs - reservedCPUs;
+}
 
-    var app = require('./app');
-    var debug = require('debug')('AsyncMeeting:server');
-    var http = require('http');
-    var https = require('https');
-    var logger = require('winston');
-    var fs = require('fs');
-
-    /**
-     * Get port from environment and store in Express.
-     */
-
-    var port = parseInt(process.env.PORT, 10) || 3000;
-    app.set('port', port);
-
-    /**
-     * Create HTTP server.
-     */
-
-    var server;
-
-    if (!(process.env.NODE_ENV)  || process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'dev-test') {
-        logger.info('Server is running in development mode!');
-        server = http.createServer(app);
-    } else {
-        logger.info('Server is running in ' + process.env.NODE_ENV + ' mode!');
-        app.use(function (req, res, next) {
-            var aYear = 60 * 60 * 24 * 365;
-// Set the Strict Transport Security header for a year
-// Also include subdomains
-            res.set('Strict-Transport-Security', 'max-age=' + aYear + ';includeSubdomains');
-            next();
-        });
-        var options = {
-            key: fs.readFileSync(nconf.get('server:https:keyFile')),
-            cert: fs.readFileSync(nconf.get('server:https:certFile'))
-        };
-        server = https.createServer(options, app);
-    }
-
-    /**
-     * Listen on provided port, on all network interfaces.
-     */
-
-    server.listen(port, function () {
-        console.log('Server listening on', port);
-    });
-    server.on('error', onError);
-    server.on('listening', onListening);
-
-
-    /**
-     * Event listener for HTTP server "error" event.
-     */
-    function onError(error) {
-        if (error.syscall !== 'listen') {
-            throw error;
-        }
-
-        // handle specific listen errors with friendly messages
-        switch (error.code) {
-            case 'EACCES':
-                console.error('Port ' + port + ' requires elevated privileges');
-                process.exit(1);
-                break;
-            case 'EADDRINUSE':
-                console.error('Port ' + port + ' is already in use');
-                process.exit(1);
-                break;
-            default:
-                throw error;
-        }
-    }
-
-    /**
-     * Event listener for HTTP server "listening" event.
-     */
-
-    function onListening() {
-        debug('Listening on port ' + server.address().port);
-    }
-}).catch(function(err){
-    console.error('Errored at start up: ' + err);
-    console.error(err.stack);
+cluster.setupMaster({
+    exec : './server/index.js' // Points to the index file you want to fork
 });
+// Fork workers.
+for (var i = 0; i < numCPUs; i += 1) {
+    cluster.fork();
+}
+cluster.on('disconnect', function(worker) {
+    // FIXME This can probably use some work.
+    console.log(worker);
+    console.error('disconnect!');
+    cluster.fork();
+});
+
+
 
