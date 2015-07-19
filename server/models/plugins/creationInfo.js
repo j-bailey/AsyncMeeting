cfe2ddda7a5c65ad0a026fc113b566a50886afc0
Joe Bailey <bailejl@yahoo.com>
2015-07-19 11:32:43 -0700

    Prepared for adding user identity to mongoose plugin fields

--- server/models/plugins/creationInfo.js
+++ server/models/plugins/creationInfo.js
@@ -1,8 +1,22 @@
 "use strict";
 
 module.exports = exports = function creationInfo(schema) {
-    schema.add({ createdOn: { type: Date, default: Date.now } });
-    // TODO add user reference to createInfo schema element
-//    schema.add({createdBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}});
+    schema.add({
+        createdOn: {type: Date, default: Date.now}
+        //createdBy: { type: String, required: true },
+        //createdById: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        //createdByUuid: { type: String, required: true }
+    });
+    schema.pre('validate', function (next) {
+        this.createdOn = undefined;
+        if (!this.isNew) {
+            return next();
+        }
+        // TODO add user identity
+        //this.createdBy = userName;
+        //this.createdByUuid = userUuid;
+        //this.createdById = userId;
+        next();
+    });
 };
 
--- server/models/plugins/modifiedOn.js
+++ server/models/plugins/modifiedOn.js
@@ -1,11 +1,18 @@
 "use strict";
 
 module.exports = exports = function modifyInfo(schema) {
-    schema.add({ modifiedOn: { type: Date, default: Date.now } });
-    // TODO add user reference to modifiedOn schema element
-//    schema.add({createdBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}});
-    schema.pre('save', function (next) {
+    schema.add({
+        modifiedOn: {type: Date, default: Date.now}
+        //modifiedBy: {type: String, default: 'UNKNOWN', required: true},
+        //modifiedById: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+        //modifiedByUuid: {type: String, required: true}
+    });
+    schema.pre('validate', function (next) {
         this.modifiedOn = Date.now();
+        // TODO add user identity
+        //this.modifiedBy = userName;
+        //this.modifiedByUuid = userUuid;
+        //this.modifiedById = userId;
         next();
     });
 };
