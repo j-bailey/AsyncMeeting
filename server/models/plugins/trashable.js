24450bc25bb1fc286f7cdedf89fa42d9a75122d5
Joe Bailey <bailejl@yahoo.com>
2015-11-02 19:48:02 -0800

    Added comments to trashable

--- server/models/plugins/trashable.js
+++ server/models/plugins/trashable.js
@@ -10,10 +10,19 @@ var Q = require('q');
  * of Meeting Areas, so it will mark on children as in our out of the trash.  This also handles other dependent items,
  * so they are managed as one unit.
  *
- * @type {trashable}
+ * @type {{init: exports.init}}
  */
 module.exports = exports = {
-    init: function (dependentEntiies) {
+    /**
+     *
+     * @param {Object[]} dependentEntities
+     * @param {string} dependentEntities[].modelName Name of the Mongoose model declared in Mongoose.model()
+     * @param {string} dependentEntities[].property Name of the property in the Mongoose model
+     * @param {string} dependentEntities[].direction Direction to traverse from the model to include parents or children (optional)
+     * @param {string} dependentEntities[].type Identifies the way to handle other entities that make up the data unit
+     * @returns {Function}
+     */
+    init: function (dependentEntities) {
         return function (schema) {
             schema.add({
                 originalName: {type: String, required: false, default: '', select: false},
@@ -42,12 +51,12 @@ module.exports = exports = {
                             doc.originalName = doc.name;
                             doc.name = doc.name + nodeUuid.v4();
                         }
-                        if (dependentEntiies) {
+                        if (dependentEntities) {
                             var parseQueryItems = function (item) {
                                 queryIds.push(item);
                             };
-                            for (i = 0; i < dependentEntiies.length; i += 1) {
-                                tId = dependentEntiies[i];
+                            for (i = 0; i < dependentEntities.length; i += 1) {
+                                tId = dependentEntities[i];
                                 if (tId.type === 'byModelId') {
                                     modelName = tId.modelName;
                                     prop = tId.property;
@@ -94,9 +103,9 @@ module.exports = exports = {
                             doc.rootTrashedItem = false;
                         }
                         query = {};
-                        if (dependentEntiies) {
-                            for (i = 0; i < dependentEntiies.length; i += 1) {
-                                tId = dependentEntiies[i];
+                        if (dependentEntities) {
+                            for (i = 0; i < dependentEntities.length; i += 1) {
+                                tId = dependentEntities[i];
                                 if (tId.type === 'byModelId') {
                                     modelName = tId.modelName;
                                     prop = tId.property;
