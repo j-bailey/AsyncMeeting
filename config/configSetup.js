1397ee245dd3b9988c8588b5e3e222015e3dd4f5
Joe Bailey <bailejl@yahoo.com>
2015-12-19 09:24:55 -0800

    Fixed redis config issue

--- config/configSetup.js
+++ config/configSetup.js
@@ -7,6 +7,6 @@ if(nconf.get('configOverrideFile')) {
     nconf.file('configOverrideFile', nconf.get('configOverrideFile')); // Allows you to inject a settings file from the command line or env
 }
 nconf.file('base', { file:'base.json', dir:__dirname, search: true}); // this holds what is needed to connect to Redis
-nconf.use('redis', { host: nconf.get('redis:host'), port: nconf.get('redis:port'), ttl: 0, db:0 });  // bulk of the environment settings
+//nconf.use('redis', { host: nconf.get('redis:host'), port: nconf.get('redis:port'), ttl: 0, db:0 });  // bulk of the environment settings
 nconf.defaults({type:'file', file:'default.json', dir:__dirname, search: true});
 
