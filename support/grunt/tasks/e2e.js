e2aaca18ae1af0cb2487189c08c573d1c8a9d4a1
Joe Bailey <bailejl@yahoo.com>
2015-12-24 09:21:20 -0800

    Added fixes to e2e tests for remote proudction system testing

--- support/grunt/tasks/e2e.js
+++ support/grunt/tasks/e2e.js
@@ -3,7 +3,7 @@
 var path = require('path');
 
 module.exports = function (grunt) {
-    grunt.registerTask('e2e', function (testBrowser, proxy, cucumberTags, isLocalDockerRun, noServer, proto, sHost, sPort, dHost, dPort) {
+    grunt.registerTask('e2e', function (testBrowser, proxy, cucumberTags, isLocalDockerRun, noServer, proto, sHost, sPort, dHost, dPort, dName) {
         var browser = (testBrowser || 'chrome').toLowerCase();
         var runInjectionProxy = proxy;
 
@@ -13,27 +13,33 @@ module.exports = function (grunt) {
             srvPort = (sPort || 3001),
             dbHost = (dHost || 'localhost'),
             dbPort = (dPort || 27017),
-            protocol = (proto || 'http');
+            protocol = (proto || 'http'),
+            dbName = (dName || 'asyncmeeting_test');
 
-        if (isLocalDockerRun) {
+
+        if (isLocalDockerRun && isLocalDockerRun.toUpperCase() === 'TRUE') {
             console.log('Testing against local Docker site');
             var dockerIp = process.env.DOCKER_HOST.split(':')[1].split('/')[2];
             console.log('Docker host IP = ' + dockerIp);
             srvHost = dockerIp;
             dbHost = dockerIp;
+            dbName = (dName || 'asyncmeeting_prod');
             protocol = (proto || 'https');
             srvPort = (sPort || 3000);
             process.env.configOverrideFile = path.normalize(path.join(__dirname, '../../../config/docker-test.json'));
         }
-        if (noServer || isLocalDockerRun) {
-            var dbNames = ['read-only', 'read-write', 'admin', 'session', 'acl'];
-            for (var i =0; i < dbNames.length; i += 1) {
-                var hostKey = 'database:' + dbNames[i] + ':host';
-                var portKey = 'database:' + dbNames[i] + ':port';
+        if ((noServer && noServer.toUpperCase() === 'TRUE') || (isLocalDockerRun && isLocalDockerRun.toUpperCase() === 'TRUE')) {
+            var dbConns = ['read-only', 'read-write', 'admin', 'session', 'acl'];
+            for (var i =0; i < dbConns.length; i += 1) {
+                var hostKey = 'database:' + dbConns[i] + ':host';
+                var portKey = 'database:' + dbConns[i] + ':port';
+                var dbNameKey = 'database:' + dbConns[i] + ':database';
                 console.info('Key ' + hostKey + ' IP ' + dbHost);
                 console.info('Key ' + portKey + ' IP ' + dbPort);
+                console.info('Key ' + dbNameKey + ' IP ' + dbName);
                 process.env[hostKey] = dbHost;
                 process.env[portKey] = dbPort;
+                process.env[dbNameKey] = dbName;
             }
             grunt.config.set('baseUrl', protocol +'://' + srvHost + ':' + srvPort);
         } else {
@@ -44,6 +50,8 @@ module.exports = function (grunt) {
             grunt.task.run(['start-external-services', 'file-launch:gulpTestServer:gulp:[\'test|server\']']);
             grunt.config.set('baseUrl', 'http://localhost:3001');
         }
+
+        console.log('NODE_ENV = ' + process.env.NODE_ENV);
 
         if (runInjectionProxy) {
             grunt.task.run(['run-injection-proxy']);
@@ -57,7 +65,7 @@ module.exports = function (grunt) {
             grunt.task.run(['protractor:' + browser + 'NoTags']);
         }
 
-        if (!isLocalDockerRun) {
+        if (!(isLocalDockerRun && isLocalDockerRun.toUpperCase() === 'TRUE') && !(noServer && noServer.toUpperCase() === 'TRUE')) {
             grunt.task.run(['file-launch-kill:gulpTestServer', 'kill-external-services']);
         }
     });
